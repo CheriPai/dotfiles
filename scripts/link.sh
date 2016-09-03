e9de6ae64e0430f0bc5ca2871272eb8723544667
dat <datdo@rocketmail.com>
2016-09-02 17:32:05 -0700

    Link redshift

--- scripts/link.sh
+++ scripts/link.sh
@@ -3,6 +3,7 @@ ln -s $HOME/.config/dotfiles/compton.conf $HOME/.config/compton.conf
 ln -s $HOME/.config/dotfiles/.gtkrc-2.0 $HOME/.gtkrc-2.0
 ln -s $HOME/.config/dotfiles/.gtkrc-2.0.mine $HOME/.gtkrc-2.0.mine
 ln -s $HOME/.config/dotfiles/.i3blocks.conf $HOME/.i3blocks.conf
+ln -s $HOME/.config/dotfiles/redshift.conf $HOME/.config/redshift.conf
 ln -s $HOME/.config/dotfiles/.vimperatorrc $HOME/.vimperatorrc
 ln -s $HOME/.config/dotfiles/.vimrc $HOME/.vimrc
 ln -s $HOME/.config/dotfiles/.Xdefaults $HOME/.Xdefaults
