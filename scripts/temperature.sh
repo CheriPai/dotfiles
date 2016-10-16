c3c4434517e7e8ee75600ecdd2b7d8b3ab4171b4
dat <datdo@rocketmail.com>
2016-10-16 14:15:37 -0700

    Add gpu temperature

--- scripts/temperature.sh
+++ scripts/temperature.sh
@@ -5,7 +5,8 @@ OUTPUT=$(sensors -Au)
 IFS=$'\n'
 DATA=$(echo "${OUTPUT}" | egrep -A4 "${CORE}")
 CURRENT=$(echo "${DATA}" | grep -Po "(?<=temp1_input: )([0-9]+)")
+GPU=$(nvidia-smi | head -9 | tail -1 | grep -Po "(?<=%   )([0-9]+)")
 
-echo "${CURRENT}°" | tail -n1
+echo "${CURRENT}° | ${GPU}°" | tail -n1
 
 exit 0
