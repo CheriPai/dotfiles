b3e2369c0b78952e0451924072ed7f8a4cf50f33
dat <datdo@rocketmail.com>
2016-12-26 23:35:37 -0800

    Use first instance of temp1_input

--- scripts/temperature.sh
+++ scripts/temperature.sh
@@ -4,7 +4,7 @@ OUTPUT=$(sensors -Au)
 
 IFS=$'\n'
 DATA=$(echo "${OUTPUT}" | egrep -A4 "${CORE}")
-CURRENT=$(echo "${DATA}" | grep -Po "(?<=temp1_input: )([0-9]+)")
+CURRENT=$(echo "${DATA}" | grep -Po -m 1 "(?<=temp1_input: )([0-9]+)")
 GPU=$(nvidia-smi | head -9 | tail -1 | grep -Po "(?<=%   )([0-9]+)")
 
 echo "${CURRENT}° | ${GPU}°" | tail -n1
